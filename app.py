68e94574bc7fcec5a0ca80d4020d06857f44fe22
Oscar Forner Martinez <violador.de.segmentos@gmail.com>
2017-08-05 11:41:56 +0100

    Add Flask-Assets to requirements
    Add translations

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, request, g, render_template
+from flask_babel import Babel
 
 app = Flask(__name__)
+babel = Babel(app)
 
-@app.route('/')
+@app.before_request
+def before():
+    if request.view_args and 'lang_code' in request.view_args and request.view_args['lang_code'] in ['es', 'ca', 'en']:
+        g.current_lang = request.view_args['lang_code']
+        request.view_args.pop('lang_code')
+    else:
+        g.current_lang = 'en'
+
+@app.route('/<lang_code>')
 def index():
     return render_template('index.html')
 
