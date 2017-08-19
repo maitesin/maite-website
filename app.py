e5c6f9dcc144ac531d369ccf729e1bba826d2893
Oscar Forner Martinez <oscar.forner@vcatechnology.com>
2017-08-19 08:35:40 +0100

    Update the flask app to support several languages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, g, render_template
+from flask import Flask, request, g, render_template, redirect, url_for, abort
 from flask_babel import Babel
 
 app = Flask(__name__)
@@ -6,15 +6,24 @@ babel = Babel(app)
 
 @app.before_request
 def before():
-    if request.view_args and 'lang_code' in request.view_args and request.view_args['lang_code'] in ['es', 'ca', 'en']:
-        g.current_lang = request.view_args['lang_code']
-        request.view_args.pop('lang_code')
-    else:
-        g.current_lang = 'en'
+    if request.view_args and 'lang_code' in request.view_args:
+        if request.view_args['lang_code'] in ['es', 'ca', 'en']:
+            g.current_lang = request.view_args['lang_code']
+            request.view_args.pop('lang_code')
+        else:
+            return abort(404)
+
+@babel.localeselector
+def get_locale():
+    return g.get('current_lang', 'en')
 
 @app.route('/<lang_code>')
 def index():
     return render_template('index.html')
+
+@app.route('/')
+def root():
+    return redirect(url_for('index', lang_code='en'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
